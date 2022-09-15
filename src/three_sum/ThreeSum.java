5374e32d97563f477325aa072db82e4ce12edd08
azrindipu <azrindipu@gmail.com>
2022-09-15 21:25:42 +0200

    15. 3Sum

---
+++ src/three_sum/ThreeSum.java
@@ -0,0 +1,17 @@
+package three_sum;
+
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args){
+        int[] nums = {-1,0,1,2,-1,-4};
+        Solution1 solution1 = new Solution1();
+        List<List<Integer>> result = solution1.threeSum(nums);
+        for(int i=0; i<result.size(); i++){
+            for(int j=0; j<result.get(i).size(); j++){
+                System.out.print(result.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
